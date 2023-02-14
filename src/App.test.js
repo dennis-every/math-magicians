dde0e2fa80248bb905a470148ed29aedb6186620
Dennis Every <dennis@ansyra.com>
2023-02-14 09:38:28 -0400

    Remove react boilerplate from src folder

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hello world', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Hello World!/);
+  expect(title).toBeInTheDocument();
 });
