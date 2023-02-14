f686b66d182f0f2325c8607ce37d10ff9d221657
Dennis Every <dennis@ansyra.com>
2023-02-14 14:54:27 -0400

    Remove other unneeded create react app files / code

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hello world', () => {
-  render(<App />);
-  const title = screen.getByText(/Hello World!/);
-  expect(title).toBeInTheDocument();
-});
