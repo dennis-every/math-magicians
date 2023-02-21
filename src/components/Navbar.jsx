2687827e25dfac55c1e6a31b107cf5f382474ed6
Dennis Every <dennis@ansyra.com>
2023-02-21 12:59:42 -0400

    Add NavLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const links = [
+  { path: '/', text: 'Home' },
+  { path: 'calculator', text: 'Calculator' },
+  { path: 'quote', text: 'Quote' },
+];
+
 const Navbar = () => (
-  <div>Navigation items here</div>
+  <nav className="navbar">
+    <ul>
+      {links.map((link) => (
+        <li key={link.text}>
+          <NavLink to={link.path}>
+            {link.text}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </nav>
 );
 
 export default Navbar;
